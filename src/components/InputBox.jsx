8637db7ccf7fd30bdcff5fd40c1d200325154425
Subham-Das-98 <das198subham@gamil.com>
2024-11-16 00:23:37 +0530

    optimize event listners

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
-function InputBox(
-  {
-    label,
-    amount,
-    onAmountChange,
-    countryCode = "usd",
-    countryCodeOptions = [],
-    currencyCountryMap = {},
-    onCountryCodeChange,
-    amountInputDisabled = false,
-    countryCodeSelectOptionDisabled = false,
-    onSelectOpenHandler,
-    onSelectCloseHandler
-  }
-) {
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  countryCode = "usd",
+  countryCodeOptions = [],
+  currencyCountryMap = {},
+  onCountryCodeChange,
+  amountInputDisabled = false,
+  countryCodeSelectOptionDisabled = false,
+  onSelectOpenHandler,
+  onSelectCloseHandler,
+}) {
   return (
     <>
-      <form action="" onSubmit={(e) => {e.preventDefault()}} name="currency-form">
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        name="currency-form"
+      >
         <div className="bg-gray-700 w-full max-w-96 mx-auto rounded-lg px-3 py-3">
           <div className="flex justify-between">
             <div className="cursor-pointer">
@@ -30,33 +34,44 @@ function InputBox(
             <input
               type="number"
               min={1}
-              className={"outline-none w-1/2 rounded-md px-2 py-1.5 bg-gray-800"}
+              className={
+                "outline-none w-1/2 rounded-md px-2 py-1.5 bg-gray-800"
+              }
               id="form-amount-input"
               placeholder="Amount"
               value={amount}
-              onChange={(e) => {onAmountChange && onAmountChange(Number(e.target.value))}}
+              onChange={(e) => {
+                onAmountChange && onAmountChange(Number(e.target.value));
+              }}
               disabled={amountInputDisabled}
             />
             <select
-              className="outline-none cursor-pointer px-4 py-1.5 rounded-md bg-gray-800"
+              className="outline-none cursor-pointer px-4 py-1.5 rounded-md bg-gray-800 select"
               value={countryCode}
-              onChange={(e) => onCountryCodeChange && onCountryCodeChange(e.target.value)}
-              onClick={(e) => {
+              onChange={(e) =>
+                onCountryCodeChange && onCountryCodeChange(e.target.value)
+              }
+              onPointerUp={(e) => {
                 onSelectOpenHandler && onSelectOpenHandler(e);
               }}
-              onMouseLeave={(e) => {
-                onSelectCloseHandler && onSelectCloseHandler(e);
-              }}
               onPointerOut={(e) => {
+                e.preventDefault()
                 onSelectCloseHandler && onSelectCloseHandler(e);
               }}
               disabled={countryCodeSelectOptionDisabled}
             >
-              {
-                countryCodeOptions.map((code) => 
-                  <option key={code} value={code} data-full-text={`${currencyCountryMap[code]}(${code.toUpperCase()})`}>{code.toUpperCase()}</option>
-                )
-              }
+              {countryCodeOptions.map((code) => (
+                <option
+                  className="option"
+                  key={code}
+                  value={code}
+                  data-full-text={`${
+                    currencyCountryMap[code]
+                  }(${code.toUpperCase()})`}
+                >
+                  {code.toUpperCase()}
+                </option>
+              ))}
             </select>
           </div>
         </div>
